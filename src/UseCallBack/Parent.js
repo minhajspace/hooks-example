1e57ad095d8c829e7d65ca1f9014787b9f8c7cd0
minhajspace <minhaj.space@gmail.com>
2021-01-06 18:03:26 +0530

    useCallback hook complete

--- src/UseCallBack/Parent.js
+++ src/UseCallBack/Parent.js
@@ -1,22 +1,27 @@
-import React,{useState} from 'react'
+import React,{useState,useCallback} from 'react'
 import Button from './Button'
 import Number from './Number'
 import Title from './Title'
 
 const ParentCompoent = () => {
-    const [number,setAge] =useState(0)
+    const [number,setNumber] =useState(0)
+    const [numberTwo,setNumberTwo] =useState(0);
 
-   const    handleIncrement = ()=>{
-       setAge(number + 1 )
-    }
-   const    handleDecrement = () => {
-        setAge(number - 1)
-    }
+   const    handleIncrement = useCallback(()=>{
+       setNumber(number + 1 )
+   },[number])
+   
+   const    handleDecrement = useCallback(()=>{
+            setNumberTwo(numberTwo - 1)
+   },[numberTwo])
+   
+    
     return (
-        <div style={{ height: '200px', width: '600px', border: '1px solid red', background: '#e1f5fe', margin: '20px' }}>
+        <div style={{ height: '300px', width: '600px', border: '1px solid red', background: '#e1f5fe', margin: '20px' }}>
             <Title/>
             <Number number={number} />
             <Button handleClick={handleIncrement}>Increment</Button>
+            <Number number={numberTwo} />
             <Button handleClick={handleDecrement}>Decrement</Button> 
         </div>
     )
